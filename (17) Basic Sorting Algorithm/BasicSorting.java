2ae836e0cac232829ea6349773c92d6093a5e5a5
Ishan Kar <iamishankar04@gmail.com>
2026-07-21 20:25:58 +0530

    17. done upto Inbuilt Sort done

--- (17) Basic Sorting Algorithm/BasicSorting.java
+++ (17) Basic Sorting Algorithm/BasicSorting.java
@@ -1,11 +1,18 @@
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class BasicSorting {
 
     public static void main(String[] args) {
-        int[] arr = {4, 5, 1, 3, 2};
+        Integer[] arr = {1, 5, 4, 3, 2};
         // int[] arr = {1,2,3,4,5};
         // bubbleSort(arr);
-        selectionSort(arr);
+        // selectionSort(arr);
+        // insertionSort(arr);
+        // Arrays.sort(arr);
+        // Arrays.sort(arr,0,3);
+        Arrays.sort(arr, Collections.reverseOrder());
         printArray(arr);
 
     }
@@ -31,18 +38,34 @@ public class BasicSorting {
     public static void selectionSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             int smallestIndex = i;
-            for(int j = i+1;j<arr.length;j++){
-                if(arr[smallestIndex]>arr[j]){
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[smallestIndex] > arr[j]) {
                     smallestIndex = j;
                 }
             }
-            int temp = arr[smallestIndex];
-            arr[smallestIndex] = arr[i];
-            arr[i] = temp; 
+            if (smallestIndex != i) {
+                int temp = arr[smallestIndex];
+                arr[smallestIndex] = arr[i];
+                arr[i] = temp;
+            }
         }
     }
 
-    public static void printArray(int[] arr) {
+    public static void insertionSort(int[] arr) {
+        // This id studied from NYC
+        //         int[] arr = {1, 5, 4, 3, 2};
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int previous = i - 1;
+            while (previous >= 0 && arr[previous] > key) {
+                arr[previous + 1] = arr[previous];
+                previous--;
+            }
+            arr[previous + 1] = key;
+        }
+    }
+
+    public static void printArray(Integer[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
