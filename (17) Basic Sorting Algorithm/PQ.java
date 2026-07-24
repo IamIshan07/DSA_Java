1d3a92e2c742d58e8488fd316c831eac7a695136
Ishan Kar <iamishankar04@gmail.com>
2026-07-25 01:48:21 +0530

    17. Basic sorting lgorithm all done on 24th July

--- (17) Basic Sorting Algorithm/PQ.java
+++ (17) Basic Sorting Algorithm/PQ.java
@@ -17,6 +17,8 @@ public class PQ {
         int[] arr = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
         // bubblesort(arr);
         // selectionSort(arr);
+        // insertionSort(arr);
+        countingSort(arr);
         printArray(arr);
     }
 
@@ -29,7 +31,7 @@ public class PQ {
     public static void bubblesort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             int count = 0;
-            for (int j = 0; j < arr.length-1-i; j++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] < arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
@@ -37,29 +39,59 @@ public class PQ {
                     count++;
                 }
             }
-            if(count == 0){
+            if (count == 0) {
                 break;
             }
         }
     }
 
-
-
-    public static void selectionSort(int[] arr){
-        for (int i = 0; i < arr.length-1; i++) {
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
             int biggestElement = i;
-            for (int j = i+1; j < arr.length; j++) {
-                if(arr[j]>arr[biggestElement]){
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] > arr[biggestElement]) {
                     biggestElement = j;
                 }
             }
             int temp = arr[i];
             arr[i] = arr[biggestElement];
             arr[biggestElement] = temp;
-            
 
         }
     }
 
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int previous = i - 1;
+            while (previous >= 0 && arr[previous] < temp) {
+                arr[previous + 1] = arr[previous];
+                previous--;
+            }
+            arr[previous + 1] = temp;
 
+        }
+    }
+
+    public static void countingSort(int[] arr) {
+        // int[] arr = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Integer.max(arr[i], max);
+        }
+
+        int[] count = new int[max + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        int j = 0;
+        for (int i = count.length-1 ; i >= 0; i--) {
+            while(count[i]>0){
+                arr[j] = i;
+                count[i]--;
+                j++;
+            }
+        }
+    }
 }
