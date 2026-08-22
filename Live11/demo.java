16a2c7284f540e544cb87d5368b02384b2a89d5b
Ishan Kar <iamishankar04@gmail.com>
2026-08-22 18:28:58 +0530

    Live class no 11 done

---
+++ Live11/demo.java
@@ -0,0 +1,12 @@
+
+public class demo {
+
+    int sum() {
+        return 2 + 2;
+    }
+
+    public static void main(String[] args) {
+        demo c = new demo();
+        System.out.println(c.sum());
+    }
+}
