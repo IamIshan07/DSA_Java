7d520c71542e3009fb638b49f8f1a476df9fffbd
Ishan Kar <iamishankar04@gmail.com>
2026-08-27 10:16:17 +0530

    22. Recursion Basics (Part -1)   Done upto 'Print nTh Fibonacci number' on 23rd August

--- (22) Recursion Basics_p1/Fibonacci.java
+++ (22) Recursion Basics_p1/Fibonacci.java
@@ -2,7 +2,7 @@
 public class Fibonacci {
 
     public static void main(String[] args) {
-        int n = 27;
+        int n = 5;
         System.out.println(fib(n));
     }
 
@@ -11,6 +11,10 @@ public class Fibonacci {
         if (n == 0 || n == 1) {
             return n;
         }
-        return fib(n - 1) + fib(n - 2);
+
+        int fnm1 = fib(n - 1);
+        int fnm2 = fib(n - 2);
+        int fn = fnm1 + fnm2;
+        return fn;
     }
 }
